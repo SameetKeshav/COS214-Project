a4d16ec3144a5cfb2712c047ef777fd61f20169a
keamothapo <u21543462@tuks.co.za>
2022-10-20 12:58:37 +0200

    Comments on the WarTheatre class

--- Code/WarTheatre.cpp
+++ Code/WarTheatre.cpp
@@ -1,11 +1,20 @@
 #include "WarTheatre.h"
 
-WarTheatre::WarTheatre() {
+WarTheatre::WarTheatre(Strategy myStrat) {
 	// TODO - implement WarTheatre::WarTheatre
-	throw "Not yet implemented";
+	//throw "Not yet implemented";
+    strategy = myStrat;
+    venue = decideVenue(strategy);
 }
 
 void WarTheatre::sendScout() {
 	// TODO - implement WarTheatre::sendScout
 	throw "Not yet implemented";
+}
+
+string decideVenue(Strategy*)
+{
+   //TODO - implement decideVenue this way:
+        //if strategy.getName == xx, then return yy
+        //do for all available strategies.
 }
