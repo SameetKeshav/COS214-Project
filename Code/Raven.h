a357e9d10644528bee4eaa8c1b67ca1f411c14d7
Morgan90-B <107586615+Morgan90-B@users.noreply.github.com>
2022-11-01 09:19:22 +0200

    intergrated sameet changes (comments)

--- Code/Raven.h
+++ Code/Raven.h
@@ -1,11 +1,15 @@
 #ifndef RAVEN_H
 #define RAVEN_H
 
+/** @brief The Abstract class for the Observer
+    A class that the Concrete Observer class inherits from
+    @author Sameet Keshav u21479373
+    @date October 2022
+    */
 class Raven {
 
 
 public:
+    /// The Pure Virtual function for the update method that the concrete class uses
 	virtual void update() = 0;
 };
-
-#endif
