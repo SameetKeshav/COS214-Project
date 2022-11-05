9ac8e9018490e3031138775f0eea1ff73c9aac70
Morgan90-B <107586615+Morgan90-B@users.noreply.github.com>
2022-11-05 21:16:23 +0200

    Final doxygen documentation for strategy pattern, state pattern, kingdom.h, and warindicators.h

--- Code/Economy.h
+++ Code/Economy.h
@@ -14,21 +14,30 @@ class Economy :public WarIndicators {
 private:
 	/**state of economy**/
 	State* state;
+	/**Monetary reserves of the Kingdom**/
 	int currency;
 
 public:
+	/** @brief Constructor. initializes State pointer and currency.
+	 * @param state - State of Economy.
+	 * @param currency - Monetary reserves of the Kingdom.
+	*/
 	Economy(State* state,int currency);
-
+	/** @todo tells State to invoke its getDemotionState method.**/
 	void SetState();
-
+	/** @todo returns State pointer.
+	 * @return State of Economy. **/
 	State* getState();
-
+	/** @todo tells State to invoke its decreaseCurrency method and then
+	 * it invokes notify method of MasterofCoin passing itself as the argument.**/
 	void decreaseCurrency();
-
+	/** @todo returns currency.
+	 * @return currency of Economy. **/
 	int getCurrency();
-
+	/** @todo reduces currency by the passed in amount.
+	 * @param i - Expense of manufacturing required Supplies.**/
 	void removeCurrency(int i);
-
+	/** @todo destructor. deallocates State.**/
 	virtual ~Economy();
 };
 #endif
--- Code/WarIndicators.h
+++ Code/WarIndicators.h
@@ -2,7 +2,7 @@
 #define WARINDICATORS_H
 #include "Treasury.h"
 class Treasury;
-/** @brief An interface class for all classes that communicate 
+/** @todo An interface class for all classes that communicate 
  * with each other through Treasury mediator.
     @author Morgan Bentley
     @date October 2022
@@ -14,19 +14,20 @@ protected:
 	Treasury* m;
 
 public:
-	/** @brief Default constructor. initializes m to null.
+	/** @todo Default constructor. initializes m to null.
 	*/
 	WarIndicators(){
 		m = nullptr;
 	}
-
-	/** @brief constructor. initializes m to the passed in Treasury object.
+	/** @todo constructor. initializes m to the passed in Treasury object.
 	 * @param m - mediator which is of Treasury pointer type
 	*/
 	WarIndicators(Treasury* m){
 		this->m=m;
 	}
-
+	/** @todo sets m (Treasury mediator pointer) to the passed in Treasury pointer.
+	 * @param m - mediator which is of Treasury pointer type
+	*/
 	void setTreasury(Treasury* m){
 		this->m=m;
 	}
